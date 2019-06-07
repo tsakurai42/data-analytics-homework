d0d46dcff02780c8d639b5b18ac14686dcad5ed8
unknown <tsakurai42@gmail.com>
2019-06-07 10:35:51 -0600

    changed date handling

--- Assignment-13-Javascript/StarterCode/static/js/app.js
+++ Assignment-13-Javascript/StarterCode/static/js/app.js
@@ -6,43 +6,22 @@ var tableData = data;
 var ufoTableBody = d3.select("#ufo-table > tbody");
 var selectCountry = d3.select("#countryselect");
 var selectState = d3.select("#stateselect");
-var filterButton = d3.select("#filter-btn")
-var dateFilter = d3.select("#datetime")
+var filterButton = d3.select("#filter-btn");
+var dateFilter = d3.select("#datetime");
 
-populate_filters();
+populate_filters(); // init setups
+filterAndShow(); // init setups
 
 function populate_filters() {
 
-    var countryList = []
-    var stateList = []
+    var countryList = [];
+    var stateList = [];
 
-    //on init, this is blank so start with "all". On change when this fxn is run, it may or may not be "all"
-    // if (selectCountry.property("value") == "") {
-    //     var countrySelected = "all"
-    // } else {
-    //     var countrySelected = selectCountry.property("value")
-    // }
-    // if (selectState.property("value") == "") { 
-    //     var stateSelected = "all"
-    // } else {
-    //     var stateSelected = selectState.property("value")
-    // }
+    selectCountryOption = selectCountry.append("option");
+    selectCountryOption.attr("value", "all").text("All").attr("selected", true);
 
-    // d3.selectAll("#countryselect").html(null);
-    // d3.selectAll("#stateselect").html(null);
-
-    selectCountryOption = selectCountry.append("option")
-    selectCountryOption.attr("value", "all").text("All").attr("selected", true)
-
-    selectStateOption = selectState.append("option")
-    selectStateOption.attr("value", "all").text("All").attr("selected", true)
-
-    // if (countrySelected != "all") {
-    //     countryList = [countrySelected]
-    // }
-    // if (stateSelected != "all") {
-    //     stateList = [stateSelected]
-    // }
+    selectStateOption = selectState.append("option");
+    selectStateOption.attr("value", "all").text("All").attr("selected", true);
 
     // ------------------------------
     // a bit of code to filter state and country based on the selections (like if CA country is selected, only ON state shows up). It turned out less user friendly because on misclick, you would have to exit
@@ -75,91 +54,64 @@ function populate_filters() {
     var tableData = data // reload full dataset
     tableData.forEach(ufodat => {
         if (!stateList.includes(ufodat.state)) {
-            stateList.push(ufodat.state)
+            stateList.push(ufodat.state);
         }
         if (!countryList.includes(ufodat.country)) {
-            countryList.push(ufodat.country)
+            countryList.push(ufodat.country);
         }
     })
 
     for (eachcountry of countryList) {
-        selectCountryOption = selectCountry.append("option")
-        selectCountryOption.attr("value", eachcountry.toLowerCase())
-        selectCountryOption.text(eachcountry.toUpperCase())
-        // if (eachcountry == countrySelected) {
-        //     selectCountryOption.attr("selected", true)
-        // }
+        selectCountryOption = selectCountry.append("option");
+        selectCountryOption.attr("value", eachcountry.toLowerCase());
+        selectCountryOption.text(eachcountry.toUpperCase());
     }
     stateList.sort();
     for (eachstate of stateList) {
-        selectStateOption = selectState.append("option")
-        selectStateOption.attr("value", eachstate.toLowerCase())
-        selectStateOption.text(eachstate.toUpperCase())
-        // if (eachstate == stateSelected) {
-        //     selectStateOption.attr("selected", true)
-        // }
+        selectStateOption = selectState.append("option");
+        selectStateOption.attr("value", eachstate.toLowerCase());
+        selectStateOption.text(eachstate.toUpperCase());
     }
 }
 
-// initial table setup
-tableData.forEach((ufoData) => {
-    var row = ufoTableBody.append("tr");
-
-    Object.entries(ufoData).forEach(([key, val]) => {
-        var cell = row.append("td");
-        var val1 = [];
-        if (key == "city") {
-            val = val.split(" ");
-            for (each_word of val) {
-                val1.push(each_word.charAt(0).toUpperCase() + each_word.slice(1));
-                val = val1.join(" ")
-            }
-        } else if (key == "country" || key == "state") {
-            val = val.toUpperCase()
-        }
-        cell.text(val)
-    })
-})
-
 //get min and max date values
 var mapped = tableData.map(ufo => {
-    return Date.parse(ufo.datetime)
+    return Date.parse(ufo.datetime);
 })
 
-var mindate = new Date(math.min(mapped))
-var maxdate = new Date(math.max(mapped))
-minDateString = `${mindate.getMonth() + 1}/${mindate.getDate()}/${mindate.getFullYear()}` //why is getMonth 0 indexed but getDate isn't...???
-maxDateString = `${maxdate.getMonth() + 1}/${maxdate.getDate()}/${maxdate.getFullYear()}`
-
-filterButton.on("click", filterAndShow)
+var mindate = new Date(math.min(mapped));
+var maxdate = new Date(math.max(mapped));
+minDateString = `${mindate.getMonth() + 1}/${mindate.getDate()}/${mindate.getFullYear()}`; //why is getMonth 0 indexed but getDate isn't...???
+maxDateString = `${maxdate.getMonth() + 1}/${maxdate.getDate()}/${maxdate.getFullYear()}`;
 
 function filterAndShow() {
     tableData = data; // reload full dataset
-    d3.event.preventDefault();
+    //d3.event.preventDefault(); // I put this on the on('change', ) instead because if I call it without a d3.event, it threw an error.
+    //But I wanted to use it for initial table set up, since it's the same code.
 
     // if countryselect dropdown is not "all", filter based on the chosen value
     if (selectCountry.property('value') != "all") {
-        tableData = tableData.filter(ufo => ufo.country == selectCountry.property('value'))
+        tableData = tableData.filter(ufo => ufo.country == selectCountry.property('value'));
     }
 
     // if stateselect dropdown is not "all", filter based on the chosen value. This filters what was already filtered if both country and state are selected
     if (selectState.property('value') != "all") {
-        tableData = tableData.filter(ufo => ufo.state == selectState.property('value'))
+        tableData = tableData.filter(ufo => ufo.state == selectState.property('value'));
     }
 
-    chosenDate = dateFilter.property('value')
-    if (chosenDate) {
-        if (Date.parse(chosenDate) < mindate || Date.parse(chosenDate) > maxdate) { // if date is not within min or max date.
-            d3.select("#iferrorhere").text(`Data only exists between ${minDateString} to ${maxDateString}`)
+    var chosenDate = new Date(dateFilter.property('value')); // Switched to using Date objects because it is much more robust
+    // it can handle different date formats and converts them all to one standard.
+    // so people entering 1/1/2010, 01/01/2010, 2010/1/1/, 1/1/10 all work.
+    if (chosenDate != "Invalid Date") {
+        if (chosenDate < mindate || chosenDate > maxdate) { // if date is not within min or max date.
+            d3.select("#iferrorhere").text(`Data only exists between ${minDateString} to ${maxDateString}`);
         } else {
-            d3.select("#iferrorhere").text("")
-            splitDate = chosenDate.split("/") // getting rid of leading zeros in case. split to list, convert list values to integers, rejoin.
-            chosenDate = [parseInt(splitDate[0]), parseInt(splitDate[1]), parseInt(splitDate[2])].join("/")
+            d3.select("#iferrorhere").text("");
         }
-        tableData = tableData.filter(date => date.datetime == chosenDate) // filter based on this date. data.js did not have any leading zeros.
+        tableData = tableData.filter(date => Date.parse(date.datetime) == Date.parse(chosenDate));
     }
 
-    ufoTableBody.html(null)
+    ufoTableBody.html(null);
     tableData.forEach((ufoData) => {
         var row = ufoTableBody.append("tr");
         Object.entries(ufoData).forEach(([key, val]) => {
@@ -169,16 +121,26 @@ function filterAndShow() {
                 var val1 = [];
                 for (each_word of val) {
                     val1.push(each_word.charAt(0).toUpperCase() + each_word.slice(1));
-                    val = val1.join(" ")
+                    val = val1.join(" ");
                 }
             } else if (key == "country" || key == "state") { // all caps state and country abbreviations.
-                val = val.toUpperCase()
+                val = val.toUpperCase();
             }
-            cell.text(val)
+            cell.text(val);
         })
 
     })
 }
 
-selectCountry.on("change", filterAndShow)
-selectState.on("change", filterAndShow)
+selectCountry.on("change", () => {
+    d3.event.preventDefault();
+    filterAndShow();
+})
+selectState.on("change", () => {
+    d3.event.preventDefault();
+    filterAndShow();
+})
+filterButton.on("click", () => {
+    d3.event.preventDefault();
+    filterAndShow();
+})
